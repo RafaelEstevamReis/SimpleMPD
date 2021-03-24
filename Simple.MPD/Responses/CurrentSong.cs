b521001bfe07b187a2dc8d70557b8f017a4ec532
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-23 22:44:11 -0300

    Add more CurrentSong info fields

--- Simple.MPD/Responses/CurrentSong.cs
+++ Simple.MPD/Responses/CurrentSong.cs
@@ -9,6 +9,20 @@ namespace Simple.MPD.Responses
     {
         public string File { get; set; }
         public string Name { get; set; }
+
+        public TimeSpan Time { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        //public Guid MUSICBRAINZ_ARTISTID { get; set; }
+        //public Guid MUSICBRAINZ_ALBUMARTISTID { get; set; }
+        public string Artist { get; set; }
+        public string AlbumArtist { get; set; }
+        public string ArtistSort { get; set; }
+        public string AlbumArtistSort { get; set; }
+        public string Title { get; set; }
+        public string Album { get; set; }
+        public string Track { get; set; }
+
         public int Pos { get; set; }
         public int Id { get; set; }
 
@@ -19,6 +33,7 @@ namespace Simple.MPD.Responses
 
         public async Task ReadAsync(StreamReader stream)
         {
+            double dVal;
             var values = Helper.ResponseHelper.ReadValuesAsync(stream);
             await foreach (var pair in values)
             {
@@ -29,6 +44,37 @@ namespace Simple.MPD.Responses
                         break;
                     case "name":
                         Name = pair.Value.Trim();
+                        break;
+
+                    case "Artist":
+                        Artist = pair.Value.Trim();
+                        break;
+                    case "AlbumArtist":
+                        AlbumArtist = pair.Value.Trim();
+                        break;
+                    case "ArtistSort":
+                        ArtistSort = pair.Value.Trim();
+                        break;
+                    case "AlbumArtistSort":
+                        AlbumArtistSort = pair.Value.Trim();
+                        break;
+                    case "Title":
+                        Title = pair.Value.Trim();
+                        break;
+                    case "Album":
+                        Album = pair.Value.Trim();
+                        break;
+                    case "Track":
+                        Track = pair.Value.Trim();
+                        break;
+
+                    case "time":
+                        dVal = double.Parse(pair.Value, System.Globalization.CultureInfo.InvariantCulture);
+                        Time = TimeSpan.FromSeconds(dVal);
+                        break;
+                    case "duration":
+                        dVal = double.Parse(pair.Value, System.Globalization.CultureInfo.InvariantCulture);
+                        Duration = TimeSpan.FromSeconds(dVal);
                         break;
 
                     case "pos":
