9519bc2cec44b6086746bde37f173f6ec871c46b
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-23 22:44:40 -0300

    Fix typo

--- Simple.MPD/Commands/Playback/Pause.cs
+++ Simple.MPD/Commands/Playback/Pause.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
+﻿using System.IO;
 using System.Threading.Tasks;
 using Simple.MPD.Interfaces;
 
@@ -12,7 +9,7 @@ namespace Simple.MPD.Commands
         public enum State
         {
             Pause,
-            Resumo,
+            Resume,
             Toggle
         }
         public State PauseState { get; set; }
