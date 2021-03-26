71072149eee8ee0fe3f9686cbc70f8675eb89103
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-25 23:55:38 -0300

    Create ListAll.cs

---
+++ Simple.MPD/Commands/MusicDatabase/ListAll.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Simple.MPD.Interfaces;
+
+namespace Simple.MPD.Commands
+{
+    public class ListAll : ICommand
+    {
+        public string CommandName => "ListAll";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.SongInfoCollection();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+                await stream.WriteAsync($"listall\n");
+        }
+    }
+}
