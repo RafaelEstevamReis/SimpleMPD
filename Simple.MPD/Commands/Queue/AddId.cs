a8d1740eb5852a20ba325983c958674a4c42db69
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-25 23:38:23 -0300

    Create AddId.cs

---
+++ Simple.MPD/Commands/Queue/AddId.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Simple.MPD.Interfaces;
+
+namespace Simple.MPD.Commands
+{
+    public class AddId : ICommand
+    {
+        public string Uri { get; }
+        public int Position { get; }
+
+        public AddId(string uri, int position = -1)
+        {
+            Uri = uri;
+            Position = position;
+        }
+
+        public string CommandName => "AddId";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.ValuesList();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sUri = Helper.EscapingHelper.Escape(Uri);
+            string sPos = Position >= 0 ? $" {Position}" : "";
+
+            await stream.WriteAsync($"add \"{sUri}\"{sPos}\n");
+        }
+    }
+}
