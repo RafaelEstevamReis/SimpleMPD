e43f2ed98a213e8c2cdc8b4d7926045b53f4daef
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-25 23:38:17 -0300

    Create Add.cs

---
+++ Simple.MPD/Commands/Queue/Add.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Simple.MPD.Interfaces;
+
+namespace Simple.MPD.Commands
+{
+    public class Add : ICommand
+    {
+        public string Uri { get; }
+        public Add(string uri)
+        {
+            Uri = uri;
+        }
+
+        public string CommandName => "Add";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sUri = Helper.EscapingHelper.Escape(Uri);
+            await stream.WriteAsync($"add \"{sUri}\"\n");
+        }
+    }
+}
