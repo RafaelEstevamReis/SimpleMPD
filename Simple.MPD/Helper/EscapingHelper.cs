cd0ca295dce122c3943f0c180f21363a437ac823
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-25 23:38:00 -0300

    Create EscapingHelper.cs

---
+++ Simple.MPD/Helper/EscapingHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace Simple.MPD.Helper
+{
+    public class EscapingHelper
+    {
+        public static string Escape(string argument)
+        {
+            // First escape backslashes
+            argument = argument.Replace("\\", "\\\\");
+            // Escape quotes
+            argument = argument.Replace("\"", "\\\"");
+            argument = argument.Replace("'", "\\'");
+
+            return argument;
+        }
+    }
+}
