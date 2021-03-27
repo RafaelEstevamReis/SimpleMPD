bc5c6eb6b1101372b2af986aa68c27dd29f94503
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-27 17:42:43 -0300

    Create StringArray.cs

---
+++ Simple.MPD/Responses/StringArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Simple.MPD.Interfaces;
+
+namespace Simple.MPD.Responses
+{
+    public class StringArray : IResponse
+    {
+        public string[] Items { get; set; }
+
+        public ICommand GetCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ReadAsync(StreamReader stream)
+        {
+            List<string> lst = new List<string>();
+            string line;
+
+            while ((line = await stream.ReadLineAsync()) != "OK")
+            {
+                lst.Add(line);
+            }
+            Items = lst.ToArray();
+        }
+    }
+}
