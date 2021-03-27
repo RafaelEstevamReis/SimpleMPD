622b76b666b8fe93191fe547e05f21fab1b1d450
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-27 17:43:06 -0300

    Add Idle command

---
+++ Simple.MPD/Commands/QueryStatus/Idle.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Simple.MPD.Interfaces;
+
+namespace Simple.MPD.Commands
+{
+    public class Idle : ICommand
+    {
+        public string CommandName => "Idle";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.StringArray();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            await stream.WriteAsync("idle\n");
+        }
+    }
+}
---
+++ Simple.MPD/IdleException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple.MPD
+{
+    internal class IdleException : Exception
+    {
+        public IdleException() 
+            : base("The connection is Idle and will not accept commands")
+        {
+        }
+    }
+}
