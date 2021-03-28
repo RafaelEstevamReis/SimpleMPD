5e2fe39609199c3c329570c0a7dc6b5a6f73affb
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-28 17:42:13 -0300

    Update Program.cs

--- Simple.MPD.Tests/Program.cs
+++ Simple.MPD.Tests/Program.cs
@@ -1,16 +1,16 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
 using Simple.MPD;
 using Simple.MPD.Commands;
 using Simple.MPD.Netwroking;
 
 Console.WriteLine("Hello World!");
-MPD mpd = new MPD(new TcpConnection("192.168.1.222"));
+
+MPD mpd = new MPD(new TcpConnection("127.0.0.1"));
 mpd.DoPingAsync().Wait();
-
 //mpd.PlayAsync().Wait();
-var playList = mpd.LsInfo("NAS/NAS/Musicas Le").Result;
-
+//var playList = mpd.LsInfo("NAS/NAS/Musicas Le").Result;
 //var stats = mpd.GetStatsAsync().Result;
 //var status = mpd.GetStatusAsync().Result;
 //var curr1 = mpd.GetCurrentSongAsync().Result;
@@ -23,7 +23,30 @@ var playList = mpd.LsInfo("NAS/NAS/Musicas Le").Result;
 //mpd.SetVolumeAsync(0);
 //mpd.SetVolumeAsync(50);
 
+var notifier = new MpdNotifier(new TcpConnection("127.0.0.1"));
+notifier.NotifyEvent += (s, ev) =>
+{
+    foreach (var evnt in ev.SystemsChanged)
+    {
+        Console.WriteLine($"Event {evnt}");
+    }
+};
+notifier.NotifyStatusChange += (s, ev) =>
+{
+    Console.WriteLine($"[{string.Join(",", ev.Systems)}] [Vol:{ev.Status.Volume}] [{ev.Status.State}] {ev.CurrentSong.SongDisplayName}");
+};
+notifier.Start();
+
+Console.WriteLine("Press enter to stop notifying");
+Console.ReadLine();
+notifier.Stop();
+
+
+
 mpd = mpd;
-
-
+mpd = mpd;
+mpd = mpd;
+mpd = mpd;
+mpd = mpd;
+mpd = mpd;
 
