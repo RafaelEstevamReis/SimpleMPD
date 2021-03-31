755f046e83947976cb1712d33abfdc658daad2df
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-30 22:58:15 -0300

    Create Delete.cs

---
+++ Simple.MPD/Commands/Queue/Delete.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Simple.MPD.Interfaces;
+
+namespace Simple.MPD.Commands
+{
+    public class Delete : ICommand
+    {
+        public string Uri { get; }
+
+        public Delete(string uri)
+        {
+            Uri = uri;
+        }
+
+        public string CommandName => "Delete";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sUri = Helper.EscapingHelper.Escape(Uri);
+            await stream.WriteAsync($"delete \"{sUri}\"\n");
+        }
+    }
+}
