524cb0cb26c9e664898382153bef13d203dd4c76
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-30 22:25:22 -0300

    Update Enums.cs

--- Simple.MPD/Enums.cs
+++ Simple.MPD/Enums.cs
@@ -12,4 +12,53 @@
         Pause,
         Stop
     }
+    public enum Tags
+    {
+        Album,
+        Albumartist,
+        Albumartistsort,
+        Albumsort,
+        Artist,
+        Artistsort,
+        Comment,
+        Composer,
+        Conductor,
+        Date,
+        Disc,
+        Genre,
+        Grouping,
+        Label,
+        Musicbrainz_albumartistid,
+        Musicbrainz_albumid,
+        Musicbrainz_artistid,
+        Musicbrainz_releasetrackid,
+        Musicbrainz_trackid,
+        Musicbrainz_workid,
+        Name,
+        Originaldate,
+        Performer,
+        Title,
+        Track,
+        Work,
+
+        Any,
+    }
+    /// <summary>
+    /// Filter match for 'find'-like Expressions
+    /// </summary>
+    public enum FilterMatch
+    {
+        /// <summary>
+        /// Compiles to '=='
+        /// </summary>
+        Equals,
+        /// <summary>
+        /// Compiles to '!='
+        /// </summary>
+        Different,
+        /// <summary>
+        /// Compiles to 'contains'
+        /// </summary>
+        Contains,
+    }
 }
