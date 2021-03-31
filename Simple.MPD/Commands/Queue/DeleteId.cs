2179958861d0f4f2bb4e4cd311942b49e1096ab6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-30 22:58:06 -0300

    Create DeleteId.cs

---
+++ Simple.MPD/Commands/Queue/DeleteId.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Simple.MPD.Interfaces;
+
+namespace Simple.MPD.Commands
+{
+    public class DeleteId : ICommand
+    {
+        public string Uri { get; }
+
+        public DeleteId(string uri)
+        {
+            Uri = uri;
+        }
+
+        public string CommandName => "DeleteId";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.ValuesList();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sUri = Helper.EscapingHelper.Escape(Uri);
+            await stream.WriteAsync($"deleteid \"{sUri}\"\n");
+        }
+    }
+}
