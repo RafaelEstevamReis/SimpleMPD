457ee4ad2d5aa2ea45f22683bf6a5c4ed38a83a3
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-30 22:35:11 -0300

    Create Search.cs

---
+++ Simple.MPD/Commands/MusicDatabase/Search.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Simple.MPD.Interfaces;
+
+namespace Simple.MPD.Commands
+{
+    public class Search : ICommand
+    {
+        public string Expression { get; }
+        public string CommandName => "Search";
+
+        public Search(string expression)
+        {
+            Expression = expression;
+        }
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.SongInfoCollection();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            var expr = Helper.EscapingHelper.Escape(Expression);
+            await stream.WriteAsync($"search \"({expr})\"\n");
+        }
+
+        //public static string ExpressionBuilder(Tags tag, FilterMatch match, string search)
+        //{
+        //    string strMatch = match switch
+        //    {
+        //        FilterMatch.Equals => "==",
+        //        FilterMatch.Different => "!=",
+        //        FilterMatch.Contains => "contains",
+        //        _ => throw new ArgumentException("Invalid FilterMatch"),
+        //    };
+
+        //    string scapedSearch = Helper.EscapingHelper.Escape(search);
+
+        //    return $"{tag} {strMatch} \"{scapedSearch}\"";
+        //}
+    }
+}
