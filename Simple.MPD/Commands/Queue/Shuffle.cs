52b28adea60fbcb03e1eadadc63a5ec9eda366fe
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-03-30 22:58:00 -0300

    Create Shuffle.cs

---
+++ Simple.MPD/Commands/Queue/Shuffle.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Simple.MPD.Interfaces;
+
+namespace Simple.MPD.Commands
+{
+    public class Shuffle : ICommand
+    {
+        public string CommandName => "Shuffle";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            await stream.WriteAsync("shuffle\n");
+        }
+    }
+}
