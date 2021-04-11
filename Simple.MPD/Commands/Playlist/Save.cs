ef357ff213940c5f57b15f30820b5914df6a036b
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:09:00 -0300

    Adds summary

--- Simple.MPD/Commands/Playlist/Save.cs
+++ Simple.MPD/Commands/Playlist/Save.cs
@@ -4,22 +4,36 @@ using System.Threading.Tasks;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Save"
+    /// </summary>
     public class Save : ICommand
     {
+        /// <summary>
+        /// Name of the playlist
+        /// </summary>
         public string Name { get; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Save";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Save(string name)
         {
             Name = name;
         }
-
-        public string CommandName => "Save";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             string sName = Helper.EscapingHelper.Escape(Name);
