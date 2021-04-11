8718b09b9e22980e5e387de9127f157fb5040a5c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 11:34:19 -0300

    Add Load

---
+++ Simple.MPD/Commands/AuxClasses/Range.cs
@@ -0,0 +1,13 @@
+﻿namespace Simple.MPD.Commands
+{
+    public class Range
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Start}:{End}";
+        }
+    }
+}
---
+++ Simple.MPD/Commands/Playlist/Load.cs
@@ -0,0 +1,35 @@
+﻿using Simple.MPD.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands
+{
+    public class Load : ICommand
+    {
+        public string Name { get; }
+        public Range Range { get; }
+
+        public Load(string name, Range range = null)
+        {
+            Name = name;
+            Range = range;
+        }
+
+        public string CommandName => "Load";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sName = Helper.EscapingHelper.Escape(Name);
+            string sRange = Range?.ToString() ?? "" ;
+            await stream.WriteAsync($"load \"{sName}\" \"{sRange}\"\n");
+        }
+    }
+}
