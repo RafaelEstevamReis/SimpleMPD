bc9c5ebbf9e420c44e8a9446d1efb2fad791e912
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:10:06 -0300

    Adds summary

--- Simple.MPD/Commands/Queue/Clear.cs
+++ Simple.MPD/Commands/Queue/Clear.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Clear"
+    /// </summary>
     public class Clear : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Clear";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("clear\n");
