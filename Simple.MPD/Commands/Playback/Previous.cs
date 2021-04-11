088a2f61827b80e8e0dbc581f80b96c66ab999d0
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:06:51 -0300

    Add summary

--- Simple.MPD/Commands/Playback/Previous.cs
+++ Simple.MPD/Commands/Playback/Previous.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Find"
+    /// </summary>
     public class Previous : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Previous";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("previous\n");
