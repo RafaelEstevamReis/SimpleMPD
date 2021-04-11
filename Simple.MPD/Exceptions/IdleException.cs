012c84db5d13c6d280536b61244d999852f08b6f
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:45:30 -0300

    Idle exception move

---
+++ Simple.MPD/Exceptions/IdleException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Simple.MPD.Exceptions
+{
+    /// <summary>
+    /// Idle exception class
+    /// </summary>
+    public class IdleException : Exception
+    {
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        public IdleException() 
+            : base("The connection is Idle and will not accept commands")
+        {
+        }
+    }
+}
