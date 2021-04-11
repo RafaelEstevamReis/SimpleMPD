034da3d45519477486e800e755182f36fb187bd6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:11:36 -0300

    Adds summary

--- Simple.MPD/Events/NotifyEventArgs.cs
+++ Simple.MPD/Events/NotifyEventArgs.cs
@@ -2,10 +2,18 @@
 
 namespace Simple.MPD.Events
 {
+    /// <summary>
+    /// Notification event
+    /// </summary>
     public class NotifyEventArgs : EventArgs
     {
+        /// <summary>
+        /// Gets changed subsystems
+        /// </summary>
         public Commands.Idle.SubSystems[] SystemsChanged { get; private set; }
-
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public NotifyEventArgs(Commands.Idle.SubSystems[] systemsChanged)
         {
             SystemsChanged = systemsChanged;
--- Simple.MPD/Events/SatusChangedEventArgs.cs
+++ Simple.MPD/Events/SatusChangedEventArgs.cs
@@ -2,12 +2,26 @@
 
 namespace Simple.MPD.Events
 {
+    /// <summary>
+    /// Changed status event args
+    /// </summary>
     public class SatusChangedEventArgs : EventArgs
     {
+        /// <summary>
+        /// Get subsystems
+        /// </summary>
         public Commands.Idle.SubSystems[] Systems { get; private set; }
+        /// <summary>
+        /// Get statuses
+        /// </summary>
         public Responses.Status Status { get; private set; }
+        /// <summary>
+        /// Gt current song
+        /// </summary>
         public Responses.SongInfo CurrentSong { get; private set; }
-
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public SatusChangedEventArgs(Commands.Idle.SubSystems[] systemsChanged, Responses.Status status, Responses.SongInfo currentSong)
         {
             Systems = systemsChanged;
