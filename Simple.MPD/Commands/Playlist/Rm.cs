f4e3e005e4da8d49ab2f2e2ddb9efc3caf1120c9
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-10 22:53:13 -0300

    Create Rm.cs

---
+++ Simple.MPD/Commands/Playlist/Rm.cs
@@ -0,0 +1,29 @@
+﻿using Simple.MPD.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands
+{
+    public  class Rm : ICommand
+    {
+        public string Name { get; }
+
+        public Rm(string name)
+        {
+            Name = name;
+        }
+
+        public string CommandName => "Rm";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sName = Helper.EscapingHelper.Escape(Name);
+            await stream.WriteAsync($"rm \"{sName}\"\n");
+        }
+    }
+}
