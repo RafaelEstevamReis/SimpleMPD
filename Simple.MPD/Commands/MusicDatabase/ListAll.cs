589d7e58d5d66de97a19c5a917dc0e1729a79fc5
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:04:59 -0300

    Add summary

--- Simple.MPD/Commands/MusicDatabase/ListAll.cs
+++ Simple.MPD/Commands/MusicDatabase/ListAll.cs
@@ -4,15 +4,27 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "ListAll" - avoid using this command
+    /// </summary>
     public class ListAll : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "ListAll";
 
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.SongInfoCollection();
         }
 
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
                 await stream.WriteAsync($"listall\n");
