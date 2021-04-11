696ecc019b4090079a233a4b77a8b6a820e62823
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:09:48 -0300

    Adds summary

--- Simple.MPD/Commands/Queue/Add.cs
+++ Simple.MPD/Commands/Queue/Add.cs
@@ -4,21 +4,36 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Find"
+    /// </summary>
     public class Add : ICommand
     {
+        /// <summary>
+        /// Uri of the song
+        /// </summary>
         public string Uri { get; }
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Add";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Add(string uri)
         {
             Uri = uri;
         }
-
-        public string CommandName => "Add";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             string sUri = Helper.EscapingHelper.Escape(Uri);
