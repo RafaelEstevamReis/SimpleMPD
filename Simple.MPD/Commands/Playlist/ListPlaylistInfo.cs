e367d276d2bac202772a66056caf38bebe1ed391
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-10 22:52:35 -0300

    Create ListPlaylistInfo.cs

---
+++ Simple.MPD/Commands/Playlist/ListPlaylistInfo.cs
@@ -0,0 +1,29 @@
+﻿using Simple.MPD.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands
+{
+    public class ListPlaylistInfo : ICommand
+    {
+        public string Name { get; }
+
+        public ListPlaylistInfo(string name)
+        {
+            Name = name;
+        }
+
+        public string CommandName => "ListPlaylistInfo";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.SongInfoCollection();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sName = Helper.EscapingHelper.Escape(Name);
+            await stream.WriteAsync($"listplaylistinfo \"{sName}\"\n");
+        }
+    }
+}
