63e7bb565e2d1b74a0b572cc0245981d4824497a
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:08:16 -0300

    Adds summary

--- Simple.MPD/Commands/Playlist/ListPlaylistInfo.cs
+++ Simple.MPD/Commands/Playlist/ListPlaylistInfo.cs
@@ -4,22 +4,36 @@ using System.Threading.Tasks;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Find"
+    /// </summary>
     public class ListPlaylistInfo : ICommand
     {
+        /// <summary>
+        /// Playlist name
+        /// </summary>
         public string Name { get; }
-
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public ListPlaylistInfo(string name)
         {
             Name = name;
         }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "ListPlaylistInfo";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.SongInfoCollection();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             string sName = Helper.EscapingHelper.Escape(Name);
