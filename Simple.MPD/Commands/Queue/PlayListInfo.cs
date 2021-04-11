6d24e7b82e4b47ed5db097fcc77bda56b1e99119
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:10:29 -0300

    Adds summary

--- Simple.MPD/Commands/Queue/PlayListInfo.cs
+++ Simple.MPD/Commands/Queue/PlayListInfo.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "PlayListInfo"
+    /// </summary>
     public class PlayListInfo : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "PlayListInfo";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.SongInfoCollection();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("playlistinfo\n");
