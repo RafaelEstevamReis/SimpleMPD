d4badd6774b61bbdb08980e34b3579e0aa0fba15
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:07:09 -0300

    Adds summary

--- Simple.MPD/Commands/Playback/Stop.cs
+++ Simple.MPD/Commands/Playback/Stop.cs
@@ -4,15 +4,27 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Stop"
+    /// </summary>
     public class Stop : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Stop";
 
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
 
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("stop\n");
