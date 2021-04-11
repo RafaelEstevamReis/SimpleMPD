2526512c1e906b8b1705385b23c667dfbb2ca5df
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:11:06 -0300

    Adds summary

--- Simple.MPD/Commands/Reflection/Config.cs
+++ Simple.MPD/Commands/Reflection/Config.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Find"
+    /// </summary>
     public class Config : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Config";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.ValuesList();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("config\n");
