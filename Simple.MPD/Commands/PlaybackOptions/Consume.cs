0402f1775cbc7fb9febe4d1243ca6ce78f28c034
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:07:22 -0300

    Adds summary

--- Simple.MPD/Commands/PlaybackOptions/Consume.cs
+++ Simple.MPD/Commands/PlaybackOptions/Consume.cs
@@ -4,22 +4,36 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
-    public  class Consume : ICommand
+    /// <summary>
+    /// Executes "Consume"
+    /// </summary>
+    public class Consume : ICommand
     {
+        /// <summary>
+        /// Comsume song mode value
+        /// </summary>
         public bool ConsumeSong { get; set; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Consume";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Consume(bool consumeSong)
         {
             ConsumeSong = consumeSong;
         }
-
-        public string CommandName => "Consume";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync($"consume { (ConsumeSong ? 1: 0) }\n");
