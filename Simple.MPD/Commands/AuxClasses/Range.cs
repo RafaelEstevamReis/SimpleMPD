177ac12ccf9151622683091d5afd1961d2c021cb
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:04:43 -0300

    Add summary

--- Simple.MPD/Commands/AuxClasses/Range.cs
+++ Simple.MPD/Commands/AuxClasses/Range.cs
@@ -1,10 +1,21 @@
 ﻿namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Represents a range of songs or entries
+    /// </summary>
     public class Range
     {
+        /// <summary>
+        /// First entry
+        /// </summary>
         public int Start { get; set; }
+        /// <summary>
+        /// End of the range, NOT included
+        /// </summary>
         public int End { get; set; }
-
+        /// <summary>
+        /// Prints the format "START:END"
+        /// </summary>
         public override string ToString()
         {
             return $"{Start}:{End}";
