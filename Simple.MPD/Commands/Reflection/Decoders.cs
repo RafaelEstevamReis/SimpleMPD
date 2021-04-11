d55addb65090cbc0617c3f9a49fd58eaffa87ad8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:11:09 -0300

    Adds summary

--- Simple.MPD/Commands/Reflection/Decoders.cs
+++ Simple.MPD/Commands/Reflection/Decoders.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Decoders"
+    /// </summary>
     public class Decoders : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Decoders";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.ValuesList();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("decoders\n");
