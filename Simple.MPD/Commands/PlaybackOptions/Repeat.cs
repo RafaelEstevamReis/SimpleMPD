c81f6d4fd27c6e967417166c464addfdaa9f275d
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:07:51 -0300

    Adds summary

--- Simple.MPD/Commands/PlaybackOptions/Repeat.cs
+++ Simple.MPD/Commands/PlaybackOptions/Repeat.cs
@@ -4,22 +4,36 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Repeat"
+    /// </summary>
     public class Repeat : ICommand
     {
+        /// <summary>
+        /// Set repeat value
+        /// </summary>
         public bool SetRepeat { get; set; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Repeat";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Repeat(bool repeat)
         {
             SetRepeat = repeat;
         }
-
-        public string CommandName => "Repeat";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync($"random { (SetRepeat ? 1 : 0) }\n");
