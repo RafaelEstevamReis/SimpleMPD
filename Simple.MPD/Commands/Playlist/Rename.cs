24c420951cb9c60a5e98ef1491a1d8d1903244cb
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:08:47 -0300

    Adds summary

--- Simple.MPD/Commands/Playlist/Rename.cs
+++ Simple.MPD/Commands/Playlist/Rename.cs
@@ -4,24 +4,41 @@ using System.Threading.Tasks;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Rename"
+    /// </summary>
     public class Rename : ICommand
     {
+        /// <summary>
+        /// Old (current) playlist name
+        /// </summary>
         public string OldName { get; }
+        /// <summary>
+        /// New playlist name
+        /// </summary>
         public string NewName { get; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Rename";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Rename(string oldName, string newName)
         {
             OldName = oldName;
             NewName = newName;
         }
-
-        public string CommandName => "Rename";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             string sOld = Helper.EscapingHelper.Escape(OldName);
