1dc3aa9d585404e3499427fbe8e9811d9eee2f25
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:06:15 -0300

    Add summary

--- Simple.MPD/Commands/Playback/Next.cs
+++ Simple.MPD/Commands/Playback/Next.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Next"
+    /// </summary>
     public class Next : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Next";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("next\n");
