1b291f629929cfb52d78d62ed3109c12f066b616
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 11:39:38 -0300

    Create PlaylistDelete.cs

---
+++ Simple.MPD/Commands/Playlist/PlaylistDelete.cs
@@ -0,0 +1,31 @@
+﻿using Simple.MPD.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands
+{
+    public  class PlaylistDelete : ICommand
+    {
+        public string Name { get; }
+        public int SongPos { get; }
+
+        public PlaylistDelete(string name, int songPos)
+        {
+            Name = name;
+            SongPos = songPos;
+        }
+
+        public string CommandName => "PlaylistDelete";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sName = Helper.EscapingHelper.Escape(Name);
+            await stream.WriteAsync($"playlistdelete \"{sName}\" {SongPos}\n");
+        }
+    }
+}
