03d79ae12e9c1ae691ba4b3c0f58af6469144ccf
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:08:27 -0300

    Adds summary

--- Simple.MPD/Commands/Playlist/Load.cs
+++ Simple.MPD/Commands/Playlist/Load.cs
@@ -7,24 +7,41 @@ using System.Threading.Tasks;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Load"
+    /// </summary>
     public class Load : ICommand
     {
+        /// <summary>
+        /// Playlist name
+        /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// Optional name to be loaded
+        /// </summary>
         public Range Range { get; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Load";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Load(string name, Range range = null)
         {
             Name = name;
             Range = range;
         }
-
-        public string CommandName => "Load";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             string sName = Helper.EscapingHelper.Escape(Name);
