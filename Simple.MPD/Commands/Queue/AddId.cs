056fb3dd30fa50c42b50acf6f4d17d35f949e6d3
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:09:52 -0300

    Adds summary

--- Simple.MPD/Commands/Queue/AddId.cs
+++ Simple.MPD/Commands/Queue/AddId.cs
@@ -4,24 +4,41 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Find"
+    /// </summary>
     public class AddId : ICommand
     {
+        /// <summary>
+        /// File Uri to be added
+        /// </summary>
         public string Uri { get; }
+        /// <summary>
+        /// Position to be added to
+        /// </summary>
         public int Position { get; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "AddId";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public AddId(string uri, int position = -1)
         {
             Uri = uri;
             Position = position;
         }
-
-        public string CommandName => "AddId";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.ValuesList();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             string sUri = Helper.EscapingHelper.Escape(Uri);
