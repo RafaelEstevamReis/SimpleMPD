84effc14b97a04598752d0d2dcc16c151a4c340b
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:10:09 -0300

    Adds summary

--- Simple.MPD/Commands/Queue/Delete.cs
+++ Simple.MPD/Commands/Queue/Delete.cs
@@ -4,22 +4,36 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Delete"
+    /// </summary>
     public class Delete : ICommand
     {
+        /// <summary>
+        /// Song uri
+        /// </summary>
         public string Uri { get; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Delete";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Delete(string uri)
         {
             Uri = uri;
         }
-
-        public string CommandName => "Delete";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             string sUri = Helper.EscapingHelper.Escape(Uri);
