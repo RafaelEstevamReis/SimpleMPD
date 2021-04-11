07372f615004cae7f04a4d24849ba1f586ad3deb
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:11:45 -0300

    Adds summary

--- Simple.MPD/Exceptions/FailureException.cs
+++ Simple.MPD/Exceptions/FailureException.cs
@@ -2,21 +2,40 @@
 
 namespace Simple.MPD.Exceptions
 {
+    /// <summary>
+    /// MPD failure error
+    /// </summary>
     public class FailureException : Exception
     {
+        /// <summary>
+        /// MPD error code
+        /// </summary>
         public int Error { get; set; }
+        /// <summary>
+        /// Command list num
+        /// </summary>
         public int CommandListNum { get; set; }
+        /// <summary>
+        /// Current command
+        /// </summary>
         public string CurrentCommand { get; set; }
-
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public FailureException()
             : base()
         { }
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public FailureException(int Error, string Message)
             : base(Message)
         {
             this.Error = Error;
         }
-
+        /// <summary>
+        /// Creates a new exception from text
+        /// </summary>
         public static FailureException FromResponseText(string Text)
         {
             // ACK [error@command_listNum] {current_command} message_text
--- Simple.MPD/Exceptions/NotConnectedException.cs
+++ Simple.MPD/Exceptions/NotConnectedException.cs
@@ -1,5 +1,8 @@
 ﻿namespace Simple.MPD.Exceptions
 {
+    /// <summary>
+    /// Not connected exception
+    /// </summary>
     public class NotConnectedException : System.IO.IOException
     {
     }
