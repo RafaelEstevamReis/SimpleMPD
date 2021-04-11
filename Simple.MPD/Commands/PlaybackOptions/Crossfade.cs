b656997ea894da6b2527075d571c4f13aa3bc90d
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:07:29 -0300

    Adds summary

--- Simple.MPD/Commands/PlaybackOptions/Crossfade.cs
+++ Simple.MPD/Commands/PlaybackOptions/Crossfade.cs
@@ -4,22 +4,36 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Crossfade"
+    /// </summary>
     public class Crossfade : ICommand
     {
+        /// <summary>
+        /// Crossfade value in seconds
+        /// </summary>
         public int XFade { get; set; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Crossfade";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Crossfade(int xFade)
         {
             XFade = xFade;
         }
-
-        public string CommandName => "Crossfade";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync($"crossfade { XFade }\n");
