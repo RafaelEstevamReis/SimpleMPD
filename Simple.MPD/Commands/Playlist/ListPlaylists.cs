93d132153cc0612807bf14813bf7effe7417ccbf
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:08:23 -0300

    Adds summary

--- Simple.MPD/Commands/Playlist/ListPlaylists.cs
+++ Simple.MPD/Commands/Playlist/ListPlaylists.cs
@@ -4,15 +4,25 @@ using System.Threading.Tasks;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "ListPlaylists"
+    /// </summary>
     public class ListPlaylists : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "ListPlaylists";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.SongInfoCollection();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync($"listplaylists\n");
