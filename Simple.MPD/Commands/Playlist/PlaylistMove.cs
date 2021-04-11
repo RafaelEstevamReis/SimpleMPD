4acd44a9bd58354495ef7d3ad9d65a78afcf0dbd
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:08:44 -0300

    Adds summary

--- Simple.MPD/Commands/Playlist/PlaylistMove.cs
+++ Simple.MPD/Commands/Playlist/PlaylistMove.cs
@@ -4,26 +4,47 @@ using System.Threading.Tasks;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "PlaylistMove"
+    /// </summary>
     public class PlaylistMove : ICommand
     {
+        /// <summary>
+        /// Playlist name
+        /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// Original position
+        /// </summary>
         public int From { get; }
+        /// <summary>
+        /// Mew position
+        /// </summary>
         public int To { get; }
 
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "PlaylistMove";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public PlaylistMove(string name, int from, int to)
         {
             Name = name;
             From = from;
             To = to;
         }
-
-        public string CommandName => "PlaylistMove";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             string sName = Helper.EscapingHelper.Escape(Name);
