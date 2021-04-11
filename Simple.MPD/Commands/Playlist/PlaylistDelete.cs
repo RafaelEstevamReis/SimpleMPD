905c1d4ed9641ca365bd120f0407b6735548b2c6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:08:34 -0300

    Adds summary

--- Simple.MPD/Commands/Playlist/PlaylistDelete.cs
+++ Simple.MPD/Commands/Playlist/PlaylistDelete.cs
@@ -4,24 +4,42 @@ using System.Threading.Tasks;
 
 namespace Simple.MPD.Commands
 {
-    public  class PlaylistDelete : ICommand
+    /// <summary>
+    /// Executes "PlaylistDelete"
+    /// </summary>
+    public class PlaylistDelete : ICommand
     {
+        /// <summary>
+        /// Playlist name
+        /// </summary>
         public string Name { get; }
+        /// <summary>
+        /// Song position to delete
+        /// </summary>
         public int SongPos { get; }
 
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "PlaylistDelete";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public PlaylistDelete(string name, int songPos)
         {
             Name = name;
             SongPos = songPos;
         }
-
-        public string CommandName => "PlaylistDelete";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             string sName = Helper.EscapingHelper.Escape(Name);
