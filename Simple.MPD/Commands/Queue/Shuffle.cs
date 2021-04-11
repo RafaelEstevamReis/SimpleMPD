2dbee5dae6153465a070dda417d0c9f3656934bd
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:10:32 -0300

    Adds summary

--- Simple.MPD/Commands/Queue/Shuffle.cs
+++ Simple.MPD/Commands/Queue/Shuffle.cs
@@ -4,14 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Shuffle"
+    /// </summary>
     public class Shuffle : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Shuffle";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("shuffle\n");
