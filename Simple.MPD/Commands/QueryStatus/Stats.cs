9f5ec4a548a446c3c25aa5bc7734f5d3480455fe
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:09:11 -0300

    Adds summary

--- Simple.MPD/Commands/QueryStatus/Stats.cs
+++ Simple.MPD/Commands/QueryStatus/Stats.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Stats"
+    /// </summary>
     public class Stats : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Stats";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Stats();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("stats\n");
