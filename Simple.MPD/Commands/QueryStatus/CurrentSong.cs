8d7d9c9c203564a36c08a1d2eafde01fb5623306
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:09:06 -0300

    Adds summary

--- Simple.MPD/Commands/QueryStatus/CurrentSong.cs
+++ Simple.MPD/Commands/QueryStatus/CurrentSong.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
-    public  class CurrentSong : ICommand
+    /// <summary>
+    /// Executes "CurrentSong"
+    /// </summary>
+    public class CurrentSong : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "CurrentSong";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.SongInfo();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("currentsong\n");
