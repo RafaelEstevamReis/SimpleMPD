45aad53926ded964b4b942029d19de24614831dc
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:04:48 -0300

    Add summary

--- Simple.MPD/Commands/Connection/Ping.cs
+++ Simple.MPD/Commands/Connection/Ping.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Ping"
+    /// </summary>
     public class Ping : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Ping";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("ping\n");
