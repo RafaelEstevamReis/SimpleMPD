89fb08a75211cd8804f2f19b1445fd4f218c0976
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:07:37 -0300

    Adds summary

--- Simple.MPD/Commands/PlaybackOptions/Random.cs
+++ Simple.MPD/Commands/PlaybackOptions/Random.cs
@@ -1,28 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Simple.MPD.Interfaces;
 using System.IO;
-using System.Text;
 using System.Threading.Tasks;
-using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Random"
+    /// </summary>
     public class Random : ICommand
     {
+        /// <summary>
+        /// Set random mode value
+        /// </summary>
         public bool SetRandom { get; set; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Random";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Random(bool random)
         {
             SetRandom = random;
         }
-
-        public string CommandName => "Random";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync($"random { (SetRandom ? 1 : 0) }\n");
