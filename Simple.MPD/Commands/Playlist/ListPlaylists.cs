e821d9eb62492a8dfb7d0291f09146697e893f53
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 11:34:09 -0300

    Create ListPlaylists.cs

---
+++ Simple.MPD/Commands/Playlist/ListPlaylists.cs
@@ -0,0 +1,21 @@
+﻿using Simple.MPD.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands
+{
+    public class ListPlaylists : ICommand
+    {
+        public string CommandName => "ListPlaylists";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.SongInfoCollection();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            await stream.WriteAsync($"listplaylists\n");
+        }
+    }
+}
