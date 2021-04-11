3c66fd2042e9c9cd53bbeb064ae3055df4d8ade8
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:10:48 -0300

    Adds summary

--- Simple.MPD/Commands/Reflection/Commands.cs
+++ Simple.MPD/Commands/Reflection/Commands.cs
@@ -4,15 +4,25 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Commands"
+    /// </summary>
     public class Commands : ICommand
     {
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Commands";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.ValuesList();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             await stream.WriteAsync("commands\n");
