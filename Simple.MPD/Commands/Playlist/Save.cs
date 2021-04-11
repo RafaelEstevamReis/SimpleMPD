1044e916e224a5b4a7695c9c40cf37f4bed2a457
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-10 22:53:20 -0300

    Create Save.cs

---
+++ Simple.MPD/Commands/Playlist/Save.cs
@@ -0,0 +1,29 @@
+﻿using Simple.MPD.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands.Playlist
+{
+    public class Save : ICommand
+    {
+        public string Name { get; }
+
+        public Save(string name)
+        {
+            Name = name;
+        }
+
+        public string CommandName => "Save";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sName = Helper.EscapingHelper.Escape(Name);
+            await stream.WriteAsync($"save \"{sName}\"\n");
+        }
+    }
+}
