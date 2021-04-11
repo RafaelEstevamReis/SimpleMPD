b438f20530b5c6e2ceddcb7872a2b9311dd35519
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-10 22:53:00 -0300

    Create Rename.cs

---
+++ Simple.MPD/Commands/Playlist/Rename.cs
@@ -0,0 +1,32 @@
+﻿using Simple.MPD.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands
+{
+    public class Rename : ICommand
+    {
+        public string OldName { get; }
+        public string NewName { get; }
+
+        public Rename(string oldName, string newName)
+        {
+            OldName = oldName;
+            NewName = newName;
+        }
+
+        public string CommandName => "Rename";
+
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            string sOld = Helper.EscapingHelper.Escape(OldName);
+            string sNew = Helper.EscapingHelper.Escape(NewName);
+            await stream.WriteAsync($"rename \"{sOld}\" \"{sNew}\"\n");
+        }
+    }
+}
