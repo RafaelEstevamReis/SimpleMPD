ef7ee391fd36fc7469da33950e2f55a274b498d4
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:06:27 -0300

    Add summary

--- Simple.MPD/Commands/Playback/Pause.cs
+++ Simple.MPD/Commands/Playback/Pause.cs
@@ -4,28 +4,54 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Pause"
+    /// </summary>
     public class Pause : ICommand
     {
+        /// <summary>
+        /// Pause states
+        /// </summary>
         public enum State
         {
+            /// <summary>
+            /// Pause playback
+            /// </summary>
             Pause,
+            /// <summary>
+            /// Resume paused playback
+            /// </summary>
             Resume,
+            /// <summary>
+            /// Toogle playbakc pause state
+            /// </summary>
             Toggle
         }
+        /// <summary>
+        /// Pause state for the current command
+        /// </summary>
         public State PauseState { get; set; }
-
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Pause";
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Pause(State pauseState)
         {
             this.PauseState = pauseState;
         }
-
-        public string CommandName => "Pause";
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.Ok();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             if (PauseState == State.Toggle)
