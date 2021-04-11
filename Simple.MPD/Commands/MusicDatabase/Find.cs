b569ed631095c632b057bdf0b91ca43c6281e9a3
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-11 15:04:54 -0300

    Add summary

--- Simple.MPD/Commands/MusicDatabase/Find.cs
+++ Simple.MPD/Commands/MusicDatabase/Find.cs
@@ -5,27 +5,44 @@ using Simple.MPD.Interfaces;
 
 namespace Simple.MPD.Commands
 {
+    /// <summary>
+    /// Executes "Find"
+    /// </summary>
     public class Find : ICommand
     {
+        /// <summary>
+        /// Expression to search/filter
+        /// </summary>
         public string Expression { get; }
+        /// <summary>
+        /// Command name
+        /// </summary>
         public string CommandName => "Find";
-
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
         public Find(string expression)
         {
             Expression = expression;
         }
-
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
         public IResponse GetResponseProcessor()
         {
             return new Responses.SongInfoCollection();
         }
-
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
         public async Task WriteAsync(StreamWriter stream)
         {
             var expr = Helper.EscapingHelper.Escape(Expression);
             await stream.WriteAsync($"find \"({expr})\"\n");
         }
-
+        /// <summary>
+        /// Builds an filter Expression
+        /// </summary>
         public static string ExpressionBuilder(Tags tag, FilterMatch match, string search)
         {
             string strMatch = match switch
