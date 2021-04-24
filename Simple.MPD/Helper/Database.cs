ff503da462dd75e3c5e0957648d006dbbecd9c81
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-24 16:42:11 -0300

    Add ReadAll

---
+++ Simple.MPD/Helper/Database.cs
@@ -0,0 +1,20 @@
+﻿namespace Simple.MPD.Helper
+{
+    public class Database : Directory
+    {
+        public Database(Directory root)
+        {
+            Name = root.Name;
+            Directories = root.Directories;
+            Files = root.Files;
+        }
+    }
+    public class Directory
+    {
+        public string Name { get; set; }
+        public Directory[] Directories { get; set; }
+        public Responses.SongInfo[] Files { get; set; }
+
+        public override string ToString() => Name;
+    }
+}
---
+++ Simple.MPD/Helper/DirectoryHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simple.MPD.Helper
+{
+    public static class DirectoryHelper
+    {
+        /// <summary>
+        /// Recursive and SLOW call
+        /// </summary>
+        public static Database ReadAll(this MPD mpd, Action<string> DirectoryProgressReport = null)
+        {
+            var result = mpd.LsInfo("").Result;
+
+            Directory rootDir = processResult(mpd, result, DirectoryProgressReport);
+            rootDir.Name = "";
+
+            return new Database(rootDir);
+        }
+
+        private static Directory processResult(MPD mpd, Responses.SongInfoCollection info, Action<string> DirectoryProgressReport)
+        {
+            List<Directory> lstDirs = new List<Directory>();
+
+            foreach (var d in info.Where(e => e.Directory != null))
+            {
+                DirectoryProgressReport?.Invoke(d.Directory);
+
+                var result = mpd.LsInfo(d.Directory).Result;
+
+                var dir = processResult(mpd, result, DirectoryProgressReport);
+                dir.Name = d.Directory;
+                lstDirs.Add(dir);
+            }
+
+            return new Directory()
+            {
+                Files = info.Where(e => e.File != null).ToArray(),
+                Directories = lstDirs.ToArray(),
+            };
+        }
+    }
+}
