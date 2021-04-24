7056297bddd7d003d730158291227dfe83c3ee6c
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-24 16:41:28 -0300

    Add Swap

---
+++ Simple.MPD/Commands/Queue/Swap.cs
@@ -0,0 +1,45 @@
+﻿using Simple.MPD.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands
+{
+    public class Swap : ICommand
+    {
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "Swap";
+        /// <summary>
+        /// First song position
+        /// </summary>
+        public int Pos1 { get; }
+        /// <summary>
+        /// Second song position
+        /// </summary>
+        public int Pos2 { get; }
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        public Swap(int pos1, int pos2)
+        {
+            Pos1 = pos1;
+            Pos2 = pos2;
+        }
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            await stream.WriteAsync($"swap {Pos1} {Pos2}\n");
+        }
+    }
+}
---
+++ Simple.MPD/Commands/Queue/SwapId.cs
@@ -0,0 +1,45 @@
+﻿using Simple.MPD.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands
+{
+    public class SwapId : ICommand
+    {
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "SwapId";
+        /// <summary>
+        /// First song position
+        /// </summary>
+        public int Id1 { get; }
+        /// <summary>
+        /// Second song position
+        /// </summary>
+        public int Id2 { get; }
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        public SwapId(int id1, int id2)
+        {
+            Id1 = id1;
+            Id2 = id2;
+        }
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            await stream.WriteAsync($"swapid {Id1} {Id2}\n");
+        }
+    }
+}
