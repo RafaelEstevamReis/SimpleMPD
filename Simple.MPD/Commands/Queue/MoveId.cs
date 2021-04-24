97f1d4515ccb8ed67bbf0506ffe7f8549d1a45d1
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-24 16:56:22 -0300

    Create MoveId.cs

---
+++ Simple.MPD/Commands/Queue/MoveId.cs
@@ -0,0 +1,45 @@
+﻿using Simple.MPD.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Simple.MPD.Commands
+{
+    public class MoveId : ICommand
+    {
+        /// <summary>
+        /// Command name
+        /// </summary>
+        public string CommandName => "MoveId";
+        /// <summary>
+        /// From song
+        /// </summary>
+        public int FromId { get; }
+        /// <summary>
+        /// To song
+        /// </summary>
+        public int ToPosition { get; }
+
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        public MoveId(int fromId, int toPosition)
+        {
+            FromId = fromId;
+            ToPosition = toPosition;
+        }
+        /// <summary>
+        /// Default Response processor
+        /// </summary>
+        public IResponse GetResponseProcessor()
+        {
+            return new Responses.Ok();
+        }
+        /// <summary>
+        /// Writes command to stream
+        /// </summary>
+        public async Task WriteAsync(StreamWriter stream)
+        {
+            await stream.WriteAsync($"move {FromId} {ToPosition}\n");
+        }
+    }
+}
