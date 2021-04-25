7524fc9342b5049a255064e45634249a5ecc85de
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-04-25 00:57:08 -0300

    Create FrameworkHelper.cs

---
+++ Simple.MPD/Helper/FrameworkHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace Simple.MPD.Helper
+{
+    public static class FrameworkHelper
+    {
+#if NET45
+        static Task s_completedTask = null;
+#endif
+
+        public static Task GetCompletedTask()
+        {
+#if NET45
+            var completedTask = s_completedTask;
+            if (completedTask == null)
+                s_completedTask = completedTask = new Task(() => { return; }); // benign initialization ----
+            return completedTask;
+#else
+            return Task.CompletedTask;
+#endif
+        }
+    }
+}
